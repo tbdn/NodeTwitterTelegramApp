c70591ea8f992e8213b76ba44dd697cd77f911c9
Tobias <tobias.baden@t-online.de>
2018-03-09 13:56:16 +0100

    Link eingefügt falls Tweet zu lang

--- twitter_app.js
+++ twitter_app.js
@@ -5,8 +5,7 @@ var Twitter = new twit(config);
 
 var params = {
     q: '#Linie3 OR #Linie7 OR #Linie10 OR #Linie17',
-    result_type: 'recent',
-    tweet_mode: 'extended'
+    result_type: 'recent'
 };
 
 Twitter.get('search/tweets', params, gotData);
@@ -15,7 +14,11 @@ function gotData(err, data, response) {
     var tweets = data.statuses;
     for(var i=0; i < tweets.length; i++) {
         if(!tweets[i].retweeted_status) {
+            if(tweets[i].truncated) {
+                console.info("Tweet - Truncated. Visit full tweet at http://www.twitter.com/statuses/" + tweets[i].id_str);
+            }
             console.log(tweets[i].text + " - " + tweets[i].created_at);
+            console.log("-------");
         }
     }
 };
