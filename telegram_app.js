0abc480b0b840e8bca9478f3b0424e26e7af8133
yannic <yannickuester@gmail.com>
2018-03-11 20:33:01 +0100

    refactored command parser structure
    TODO:
    fix multiple of the same messages
    fix offset finding before reacting to messages

--- telegram_app.js
+++ telegram_app.js
@@ -2,10 +2,9 @@ const https = require('https');
 const TIMER = 1000;
 const TELEGRAM_TOKEN = require('./configs/telegram').telegram_token;
 const TELEGRAM_BASE_URL = 'https://api.telegram.org/bot'+TELEGRAM_TOKEN;
-var debugger_chat_ids = new Set();
 
 var lines = [];
-
+var activeUsers = new Set();
 var offset = -1;
 console.log("Bot online");
 generateOffset();
@@ -13,15 +12,8 @@ console.log("Offset found: "+offset);
 setInterval(getUpdates, TIMER);
 
 /**
- * update the alerts for users
- */
-function update(){
-
-}
-/**
  * get the last updates from telegram
  */
-
 function generateOffset(){
     https.get(TELEGRAM_BASE_URL+'/getUpdates', function(response) {
 
@@ -72,25 +64,14 @@ function parseUpdates(results){
     if(results == null || results.length == 0)return;
     if(offset = 0)offset = results[results.length-1].update_id+1;
     //console.log("Parsing results: "+JSON.stringify(results, null, 4));
-    //get debuggers
     for (key in results){
         result = results[key];
         console.log(JSON.stringify(result, null, 4));
-        /*
-        if(result.message.text == '/debug_start'){
-            console.log("new debugger joined: "+result.message.from.username);
-            debugger_chat_ids.add(result.message.chat.id);
-        }
-        else if(result.message.text == '/debug_stop'){
-            console.log("debugger left: "+result.message.from.username);
-            debugger_chat_ids.delete(result.message.chat.id);
-        }
-        else if(result.message.text == '/help'){
-            console.log("sending help to: "+result.message.from.username);
-            message(result.message.chat.id, "This bot is currently not functional and will only respond to this command (maybe)");
-        }
-        */
-        if(result.message.text.includes("/addLines")){
+
+        raw = result.message.text.split(" ");
+        command(result.message.chat.id,raw[0],raw.slice(1));
+
+        /*if(result.message.text.includes("/addLines")){
             console.log("adding lines");
             lines_raw = result.message.text.replace("/addLines","").replace(" ","").split(",");
             for(i of lines_raw){
@@ -111,24 +92,67 @@ function parseUpdates(results){
         }
         else{
             message(result.message.chat.id, "Unbekannter Command von "+result.message.from.first_name);
-        }
-    }
-    for (key in results){
-        console.log(key);
-        console.log(debugger_chat_ids);
-        result = results[key];
-        for(debugger_id of debugger_chat_ids.values()){
-            console.log("sending debug data to "+debugger_id);
-            message(debugger_id, result.message.from.first_name+" "+result.message.from.last_name+": "+result.message.text);
-        }
+        }*/
     }
     offset = results[results.length-1].update_id+1;
 }
 
+/**
+ * Parse a single command and react
+ * @param user chat id of the user that issued the command
+ * @param command command text, first part of the message text
+ * @param parameters remaining part of the message, split at spaces
+ */
+function command(user, command, parameters){
+    switch(command){
+        case "/add":
+            for(line of parameters){
+                addLine(user, line);
+            }
+            message(user, "Added "+parameters.toString().replace(" ",",")+" to your subscribed lines.");
+            break;
+        case "/remove":
+            for(line of parameters){
+                removeLine(user, line);
+            }
+            message(user, "Removed "+parameters.toString().replace(" ",",")+" from your subscribed lines.");
+            break;
+        case "/active":
+            activeUsers.add(user);
+            message(user, "Notifications activated.");
+            break;
+        case "/pause":
+            activeUsers.remove(user);
+            message(user, "Notifications paused. Use `/active` to reactivate notifications.");
+            break;
+        case "/help":
+            message(user, "This bot can be used to get informations about problems with uestra lines."
+                +"Use `/add` to subscribe to one or multiple lines and `/remove` to remove them."
+                +"You can use `/active` and `/pause` to activate or pause notifications.");
+            break;
+        case "/subscribed":
+            message(user, "You are currently subscribed to: "+getLinesForUser(user));
+            break;
+        case "/alert":
+            for(line of parameters){
+                alert(line, "Alarm für Cobra "+line);
+            }
+            message(user, "The subscribers of the lines "+parameters.toString().replace(" ",",")+" have been alerted.");
+            break;
+        default:
+            message(user, "Unknown command.");
+            break;
+    }
+}
+
 function alert(line, text) {
-    console.log("lines: "+JSON.stringify(lines));
+    if(lines[line] == null || lines[line] == undefined)return;
     for(id of lines[line]){
-        message(id, text);
+        console.log("Trying to alert "+id);
+        if(activeUsers.has(id)){
+            console.log("Alerting "+id);
+            message(id, text);
+        }
     }
 }
 
@@ -137,6 +161,21 @@ function addLine(user_id, line){
         lines[line] = [];
     }
     lines[line].push(user_id);
+}
+
+function removeLine(user_id, line){
+    if(lines[line] == undefined || lines[line] == null || lines[line].indexOf(user_id) == -1){
+        return;
+    }
+    lines[line].splice(lines[line].indexOf(user_id),1);
+}
+
+function getLinesForUser(user_id){
+    userLines = [];
+    for(line in lines){
+        if(lines[line] != undefined && lines[line].includes(user_id))userLines.push(line);
+    }
+    return userLines;
 }
 
 function message(chat_id, text){
